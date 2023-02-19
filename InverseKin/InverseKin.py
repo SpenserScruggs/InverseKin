e2b4a366e6fc030f9ca5df05807abdc6a4e4df73
SpenserScruggs <scru7556@vandals.uidaho.edu>
2023-02-18 18:28:24 -0800

    fixed kinematics + added a graphing feature for validation

--- InverseKin/InverseKin.py
+++ InverseKin/InverseKin.py
@@ -3,36 +3,56 @@ import sympy as sp
 import matplotlib.pyplot as plt
 
 
+
 l1 = 1
 l2 = 1
 l3 = 1
 
-xinput = float(input("X: "))
-yinput = float(input("Y: "))
-zinput = float(input("Z: "))
-
-
-# Potential inverse kinematics https://www.ijcaonline.org/archives/volume179/number37/farman-2018-ijca-916848.pdf
-
 # inverse kinematics
 def getangles(x, y, z):
     global l1, l2, l3
+
     rp = np.sqrt(x**2 + y**2 + (z - l1)**2)
     th1 = np.arctan2(y, x)
-    th3 = np.pi - np.arccos((rp**2 - l3**2 - l1**2) / (-2*l3*l2))
-    th2 = np.arctan2(z - l1, np.sqrt(x**2 + y**2)) + np.arcsin((l3 * np.sin(np.pi - th3)) / np.sqrt(x**2 + y**2 + (z - l1)**2))
-    #th3 = th2 + np.pi/2 - th3
+    B = np.arccos((rp**2 - l3**2 - l1**2) / (-2*l3*l2))
+    th3 = np.pi - B
+    th2 = np.arcsin((z-l1) / rp) + np.arctan2((l3*np.sin(th3)), (l2 + l3 * np.cos(th3)))
+    
     return [th1, th2, th3]
 
 # forward kinematics
-def getpos(th1, th2, th3):
-    global l1, l2, l3
-    x = (l2*np.cos(th2) + l3*np.cos(th2 + th3))*np.cos(th1)
-    y = (l2*np.cos(th2) + l3*np.cos(th2 + th3))*np.sin(th1)
-    z = l1 - l2*np.sin(th2) - l3*np.sin(th2 + th3)
+def getpos(angles):
+    global l1, l2, l3 
+    
+    th1 = angles[0]
+    th2 = angles[1]
+    th3 = angles[2]
+
+    r = l2 * np.cos(th2) + l3 * np.cos(th2 - th3)
+    x = r * np.cos(th1)
+    y = r * np.sin(th1)
+    z = l2 * np.sin(th2) + l3 * np.sin(th2 - th3)
     return [x, y, z]
 
-angles1 = getangles(xinput, yinput, zinput)
-print(np.multiply(180 / np.pi, angles1))
+def rad_degrees(angles):
+    return [angles[0] * 180 / np.pi, angles[1] * 180 / np.pi, angles[2] * 180 / np.pi]
 
-print(getpos(angles1[0], angles1[1], angles1[2]))
+dimensions = 20
+plt.grid(linestyle="--")
+for i in range(dimensions + 1):
+    for j in range(dimensions + 1):
+        x = 2 * i / dimensions
+        y = 0
+        z = 4 * j / dimensions
+        #print( "X:", x, " Y:", y, " Z:", z, "  ", rad_degrees(getangles(x, y, z)), getpos(getangles(x, y, z)))
+        plt.scatter(x, z, 50, color="red", marker="*", label="input points")
+        plt.scatter(getpos(getangles(x, y, z))[0], getpos(getangles(x, y, z))[2] + 1, 25, color="blue", marker="+", label="output points")
+
+xcircle = []
+ycircle = []
+for i in range(100):
+    xcircle.append(2 * np.cos(i / 30 - np.pi / 2))
+    ycircle.append(2 * np.sin(i / 30 - np.pi / 2) + 1)
+
+plt.plot(xcircle, ycircle)
+plt.show()
